83a44c3e7e99678a2a3df6ec436e1597c722db8a
gx-white <gaoxi@iie.ac.cn>
2019-11-06 09:51:16 +0800

    GetLeastNumbers
    时隔这么久...终于又更新一波。

--- AimAtOffer/GetLeastNumbers/main.py
+++ AimAtOffer/GetLeastNumbers/main.py
@@ -55,6 +55,7 @@ class Solution:
                 break
         return tinput[0:k]
 '''
+'''
 class Solution:
     def GetLeastNumbers_Solution(self, tinput, k):
         # write code here
@@ -93,7 +94,104 @@ class Solution:
             self.QuickSort(tinput, i+1, end, k)
         else:
             return
+'''
+'''
+class Solution:
+    def GetLeastNumbers_Solution(self, tinput, k):
+        # write code here
+        size = len(tinput)
+        if(size == 0):
+            return []
+        if(k > size):
+            return []
+        for i in range(0, size):   # 每次选一个最大的出来，与最后一位交换
+            self.maxHeapify(tinput, size-i)
+            # 交换
+            temp = tinput[0]
+            tinput[0] = tinput[size-i-1]
+            tinput[size-i-1] = temp
+        return tinput[0:k]
+
+    def maxHeapify(self, tinput, size):
+        # 完成一次建堆
+        for i in range((size-1)/2, -1, -1):
+            self.heapify(tinput, i, size)
+    # 建堆
+    def heapify(self, tinput, currentRootNode, size):
+        if(currentRootNode < size):
+            left = currentRootNode * 2 + 1
+            right = currentRootNode * 2 + 2
+
+            max_index = currentRootNode
+
+            if(left < size):
+                if(tinput[left] > tinput[max_index]):
+                    max_index = left
+            if(right < size):
+                if(tinput[right] > tinput[max_index]):
+                    max_index = right
+
+            if(max_index != currentRootNode):
+                temp = tinput[max_index]
+                tinput[max_index] = tinput[currentRootNode]
+                tinput[currentRootNode] = temp
+'''
+# 基于堆排序
+class Solution:
+    def GetLeastNumbers_Solution(self, tinput, k):
+        # write code here
+        size = len(tinput)
+        if(size == 0):
+            return []
+        if(k > size):
+            return []
+        if(k == 0):
+            return []
+
+        heap = tinput[0:k]
+        self.maxHeapify(heap, k)
+        for i in range(k, size):
+            if(tinput[i] >= heap[0]):
+                pass
+            else:
+                heap[0] = tinput[i]
+                self.maxHeapify(heap, k)
+        for i in range(0, k):
+            self.maxHeapify(heap, k-i)
+
+            temp = heap[0]
+            heap[0] = heap[k-i-1]
+            heap[k-i-1] = temp
+        return heap
+
+
+    def maxHeapify(self, tinput, size):
+        # 完成一次建堆，堆大小为k
+        for i in range((size-1)/2, -1, -1):
+            self.heapify(tinput, i, size)
+
+    # 建堆
+    def heapify(self, tinput, currentRootNode, size):
+        if(currentRootNode < size):
+            left = currentRootNode * 2 + 1
+            right = currentRootNode * 2 + 2
+
+            max_index = currentRootNode
+
+            if(left < size):
+                if(tinput[left] > tinput[max_index]):
+                    max_index = left
+            if(right < size):
+                if(tinput[right] > tinput[max_index]):
+                    max_index = right
+
+            if(max_index != currentRootNode):
+                temp = tinput[max_index]
+                tinput[max_index] = tinput[currentRootNode]
+                tinput[currentRootNode] = temp
+    
+
 
 if __name__ == "__main__":
     sol = Solution()
-    print(sol.GetLeastNumbers_Solution([4,5,1,6,2,7,3,8], 5))
+    print(sol.GetLeastNumbers_Solution([4,5,1,6,2,7,3,8], 8))
